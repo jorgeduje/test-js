8a7bc5ffff6dd7fb67c8cf85921357f067590fa9
jorgeduje <jorgeduje4@gmail.com>
2025-09-01 21:33:31 -0300

    fix

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("todo okey");
+
+const getProducts = async () => {
+  const response = await fetch("./data.json");
+  const res = await response.json();
+  console.log(res);
+};
+
+getProducts();
